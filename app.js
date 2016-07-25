8033a2c38cbbe981f60cfd5aeb41c800a6b00386
Pavel Gordeyev <nirrhcp41@users.noreply.github.com>
2016-07-25 19:19:34 -0400

    Add converter for unix timestamp to date string

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const hostname = '127.0.0.1';
 const port = 8080;
 
 const server = http.createServer(function(request, response){
-
+	var url = request.url.replace("/","");
+	if(/^\d+$/.test(url)){
+		console.log(url + ": " + true);
+		console.log(timestamp.unixToDateString(url));
+	}else{
+		console.log(url + ": " + false);
+	}
 }).listen(port, hostname, function(){
   console.log('Server running at http://${' + hostname + '}:${' + port + '}/');
 });
--- timestamp.js
+++ timestamp.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// Convert unix time to a date string
+function unixToDateString(unix){
+    var months=['January','February','March','April','May','June','July','August','September','October','November','December'],
+        d = new Date(parseInt(unix)),
+        dateString = months[d.getMonth()] + ' ' + d.getDate() + ',' + d.getFullYear();
+
+    return dateString;
+}
+
+module.exports.unixToDateString = unixToDateString;
