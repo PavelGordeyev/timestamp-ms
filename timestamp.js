4e19e6e989f84b875fa576691ca1440f49e42b96
Pavel Gordeyev <nirrhcp41@users.noreply.github.com>
2016-07-25 20:17:41 -0400

    Fix bug where unix time was in seconds

--- timestamp.js
+++ timestamp.js
@@ -1,18 +1,25 @@
 'use strict';
 
 // Checks if request is a valid date or unix timestamp
-function checkRequest(url){
-    if(url === ""){
+function checkRequest(uri){
+    if(uri === ""){
         return false;
     }
 
     return true;
 }
 
+// Converts regular date to proper query
+function convertURI(uri){
+    var uriArr = uri.split(' ');
+    console.log(uriArr);
+}
+
 // Convert unix time to a date string
 function unixToDateString(unix){
     var months=['January','February','March','April','May','June','July','August','September','October','November','December'],
-        d = new Date(parseInt(unix)),
+        d = new Date(parseInt(unix) * 1000),
+        dateString;
         dateString = months[d.getMonth()] + ' ' + d.getDate() + ',' + d.getFullYear();
 
     return dateString;
@@ -22,18 +29,18 @@ function unixToDateString(unix){
 function getTimeStamp(request,response){
     var unix,
         natural,
-        url = request.url.replace("/","");
+        uri = request.url.replace("/","");
 
-    if(!checkRequest(url)){
+    if(!checkRequest(uri)){
         unix = null;
         natural = null;
     }else{
-        if(/^\d+$/.test(url)){
-            unix = parseInt(url);
-            natural = unixToDateString(url);
+        if(/^\d+$/.test(uri)){
+            unix = parseInt(uri);
+            natural = unixToDateString(uri);
         }
     }
-
+    convertURI(uri);
     // Write the response to the page
     response.writeHead(200,{'Content-Type': 'text/plain'});
     response.write(JSON.stringify({"unix": unix,"natural": natural}));
