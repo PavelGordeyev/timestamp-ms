2d9b098a4a7aa2b10b0aece73167ed8d79adc1a5
Pavel Gordeyev <nirrhcp41@users.noreply.github.com>
2016-07-26 20:37:06 -0400

    Update unix calculator to display time at 12:00AM (UTC). Allow for users to input the month word with a minimum of 3 characters.

--- timestamp.js
+++ timestamp.js
@@ -12,22 +12,27 @@ function checkRequest(uri){
     return true;
 }
 
-// Converts regular date to proper query
-function convertURI(uri){
-    var uriArr = uri.split(' ');
-    return uriArr;
+// Returns the months as a full string
+function getMonth(dateArr){
+    return months.filter(function(el){
+        return el.substr(0,dateArr[0].length).indexOf(dateArr[0]) >= 0;
+    })[0];    
 }
 
+// Converts regular date into array, separated on spaces
+function convertDateUri(dateUri){
+    var dateArr = dateUri.split(' ');
+
+    // Fix month input automatically
+    dateArr.splice(0,1,getMonth(dateArr));
+
+    return dateArr;
+}
+
+// Converts a date array to unix time 12:00AM (UTC)
 function dateToUnix(dateArr){
-    var month = months.filter(function(el){
-        return el.substr(0,dateArr[0].length).indexOf(dateArr[0]) >= 0;
-    });
-
-    console.log(month);
-
-    var d = new Date(dateArr[2],months.indexOf(month[0]),dateArr[1].substr(0,dateArr[1].length-1),0,0,0,0);
-
-    return d.valueOf() / 1000;
+    var d = new Date(dateArr[2],months.indexOf(getMonth(dateArr)),dateArr[1].substr(0,dateArr[1].length-1),0,0,0,0);
+    return d.valueOf() / 1000 - 18000;
 }
 
 // Convert unix time to a date string
@@ -43,34 +48,36 @@ function getTimeStamp(request,response){
     var unix,
         natural,
         uri = request.url.replace("/",""),
-        arr,
-        encoded,
-        decoded = decodeURIComponent(uri);
+        dateArr = convertDateUri(decodeURIComponent(uri)),
+        commonHeader = {'Content-Type': 'text/plain'};
 
-    console.log(decoded);
     if(!checkRequest(uri)){
         unix = null;
         natural = null;
     }else{
         if(/^\d+$/.test(uri)){
+            // Get the unix time
             unix = parseInt(uri);
+            // Get the full date string
             natural = unixToDateString(uri);
-            response.writeHead(200,{'Content-Type': 'text/plain'});
+
+            response.writeHead(200,commonHeader);
         }else{
-            if(redirectBool){ // Redirected page
-                response.writeHead(200,{'Content-Type': 'text/plain'});
-                unix = dateToUnix(convertURI(decoded));
-                natural = decoded;
+            if(redirectBool){ // Redirected page                
+                // Get the unix time
+                unix = dateToUnix(dateArr);
+                // Get the full date string
+                natural = dateArr.join(' ');
+
+                response.writeHead(200,commonHeader);
             }else{ // Redirect to encode page
-                //encoded = encodeURI(decoded);
                 redirectBool = true;
-                response.writeHead(303, {'Location': '/' + encodeURI(decoded)});
+                response.writeHead(303, {'Location': '/' + encodeURI(dateArr.join(' '))});
             }       
         }
     }
-    //convertURI(uri);
+
     // Write the response to the page
-    
     response.write(JSON.stringify({"unix": unix,"natural": natural}));
     response.end();
 }
